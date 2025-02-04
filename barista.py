76ba57bde1d7e6a63de90923bcff531c7251b57d
Carsten Thiel <cat@schildwaechter.io>
2025-02-04 20:49:27 +0100

    Formatting

--- barista.py
+++ barista.py
@@ -21,28 +21,39 @@ from gelfformatter import GelfFormatter
 
 import mysql.connector
 
+
 # create the X-REQUEST-ID template
 hostname = socket.gethostname()
-all_letters=string.ascii_lowercase
+all_letters = string.ascii_lowercase
 random_string = ''.join(random.choice(all_letters) for i in range(6))
-request_str_template = hostname+'-'+random_string+'-'
+request_str_template = hostname + '-' + random_string + '-'
 request_counter = 0
+
+
 def get_request_id():
     global request_counter
-    request_counter+=1
-    return(request_str_template+str(request_counter))
+    request_counter += 1
+    return (request_str_template + str(request_counter))
 
-#patch logger for _extra data - https://stackoverflow.com/a/59176750
+# patch logger for _extra data - https://stackoverflow.com/a/59176750
+
+
 def make_record_with_extra(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None, sinfo=None):
     record = original_makeRecord(self, name, level, fn, lno, msg, args, exc_info, func, extra, sinfo)
     record._extra = extra
     return record
+
+
 original_makeRecord = logging.Logger.makeRecord
 logging.Logger.makeRecord = make_record_with_extra
+
+
 class myPlainFormatter(logging.Formatter):
     def format(self, record):
         return super().format(record)
-#configure logger
+
+
+# configure logger
 if "GELF_LOGGING" in os.environ:
     formatter = GelfFormatter()
 else:
@@ -50,39 +61,40 @@ else:
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(formatter)
-python_debug = os.environ.get('PYTHON_DEBUG','FALSE')
+python_debug = os.environ.get('PYTHON_DEBUG', 'FALSE')
 if python_debug == 'TRUE':
     logging.basicConfig(level=logging.DEBUG, handlers=[handler])
 else:
     logging.basicConfig(level=logging.INFO, handlers=[handler])
 
-logging.debug("# SchildCafe Barista started at "+datetime.now().isoformat())
+logging.debug("# SchildCafe Barista started at " + datetime.now().isoformat())
 
-provided_coffee_machines = json.loads(os.environ.get('COFFEE_MACHINES','["http://localhost:1337"]'))
+provided_coffee_machines = json.loads(os.environ.get('COFFEE_MACHINES', '["http://localhost:1337"]'))
 coffee_machines = []
 # resolve all hostnames to IPs to support kubernetes deployements
 for entry in provided_coffee_machines:
-    logging.debug("IP resolving coffee machine "+entry)
+    logging.debug("IP resolving coffee machine " + entry)
     old_url = urlparse(entry)
     # get IP addresses
     result = dns.resolver.resolve(old_url.hostname)
     for ipval in result:
         url_lst = list(old_url)
         if old_url.port != None:
-            url_lst[1] = ipval.to_text()+':'+str(old_url.port)
+            url_lst[1] = ipval.to_text() + ':' + str(old_url.port)
         else:
             url_lst[1] = ipval.to_text()
-        logging.debug("entry updated to "+urlunparse(url_lst))
+        logging.debug("entry updated to " + urlunparse(url_lst))
         coffee_machines.append(urlunparse(url_lst))
 
 MYSQL_USER = os.environ['MYSQL_USER']
 MYSQL_PASS = os.environ['MYSQL_PASS']
-MYSQL_HOST = os.environ.get('MYSQL_HOST','localhost')
-MYSQL_PORT = os.environ.get('MYSQL_PORT','3306')
-MYSQL_DB = os.environ.get('MYSQL_DB','cafe')
+MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+MYSQL_PORT = os.environ.get('MYSQL_PORT', '3306')
+MYSQL_DB = os.environ.get('MYSQL_DB', 'cafe')
 
 mysqldb = peewee.MySQLDatabase(MYSQL_DB, user=MYSQL_USER, password=MYSQL_PASS,
-                         host=MYSQL_HOST, port=int(MYSQL_PORT))
+                               host=MYSQL_HOST, port=int(MYSQL_PORT))
+
 
 class Order(peewee.Model):
     ID = peewee.CharField(column_name='id')
@@ -95,6 +107,7 @@ class Order(peewee.Model):
     class Meta:
         table_name = 'orders'
         database = mysqldb
+
 
 class CoffeeListItem(peewee.Model):
     ID = peewee.CharField(column_name='id')
@@ -110,51 +123,55 @@ class CoffeeListItem(peewee.Model):
         table_name = 'coffee_list_items'
         database = mysqldb
 
+
 # retrieve finished jobs
 for job in CoffeeListItem.select().where(CoffeeListItem.Machine.is_null(False) & CoffeeListItem.JobRetrieved.is_null(True)):
-    logging.debug("checking job "+job.ID)
+    logging.debug("checking job " + job.ID)
     # get comparable timestamps
     jobReady = datetime.fromisoformat(job.JobReady).isoformat(timespec='seconds')
     present = datetime.utcnow().isoformat(timespec='seconds')
     if (jobReady < present):
         this_request_id = get_request_id()
-        logging.debug("trying to receive job "+job.ID, extra={"x-request-id":this_request_id})
-        response = requests.get(job.Machine+"/retrieve-job/"+job.ID, headers = {"X-Request-ID": this_request_id})
+        logging.debug("trying to receive job " + job.ID, extra={"x-request-id": this_request_id})
+        response = requests.get(job.Machine + "/retrieve-job/" + job.ID, headers={"X-Request-ID": this_request_id})
         jsonResponse = response.json()
         job.JobRetrieved = datetime.utcnow().isoformat(timespec='seconds')
         job.save()
-        logging.info("job "+job.ID+" retrieved from "+job.Machine+" at "+job.JobRetrieved, extra={"x-request-id":this_request_id})
+        logging.info("job " + job.ID + " retrieved from " + job.Machine + " at " + job.JobRetrieved,
+                     extra={"x-request-id": this_request_id})
         # update progress counter
         order = Order.select().where(Order.ID == job.OrderID).get()
         order.OrderBrewed += 1
         # if we're done, mark the time
         if order.OrderSize == order.OrderBrewed:
             order.OrderReady = job.JobRetrieved
-            logging.info("order "+order.ID+" ready")
+            logging.info("order " + order.ID + " ready")
         order.save()
-
 
 # find empty machines
 for pot in coffee_machines:
     this_request_id = get_request_id()
-    logging.debug("getting machine status for "+pot, extra={"x-request-id":this_request_id})
-    machineStatus = requests.get(pot+"/status", headers = {"X-Request-ID": this_request_id})
+    logging.debug("getting machine status for " + pot, extra={"x-request-id": this_request_id})
+    machineStatus = requests.get(pot + "/status", headers={"X-Request-ID": this_request_id})
     if (machineStatus.status_code == 200):
-        logging.debug("looking for a job to submit to machine "+pot)
+        logging.debug("looking for a job to submit to machine " + pot)
         # get one job to schedule on the machine
         for job in CoffeeListItem.select().where(CoffeeListItem.Machine.is_null(True)).limit(1):
             this_request_id = get_request_id()
-            logging.debug("trying to start job "+job.ID+" on machine "+pot, extra={"x-request-id":this_request_id})
-            response = requests.post(pot+"/start-job", data=json.dumps({"product": job.Product}), headers={"Content-Type":"application/json","X-Request-ID": this_request_id})
+            logging.debug("trying to start job " + job.ID + " on machine " + pot, extra={"x-request-id": this_request_id})
+            response = requests.post(pot + "/start-job", data=json.dumps({"product": job.Product}),
+                                     headers={"Content-Type": "application/json", "X-Request-ID": this_request_id})
             jsonResponse = response.json()
             job.Machine = pot
-            logging.debug("submitted job "+job.ID+" has (new) jobID "+jsonResponse["jobId"], extra={"x-request-id":this_request_id})
+            logging.debug("submitted job " + job.ID + " has (new) jobID " + jsonResponse["jobId"],
+                          extra={"x-request-id": this_request_id})
             job.ID = jsonResponse["jobId"]
             job.JobReady = jsonResponse["jobReady"]
-            logging.info("job "+job.ID+" sent to "+job.Machine+", ready at "+job.JobReady, extra={"x-request-id":this_request_id})
+            logging.info("job " + job.ID + " sent to " + job.Machine + ", ready at " + job.JobReady,
+                         extra={"x-request-id": this_request_id})
             job.JobStarted = datetime.utcnow().isoformat(timespec='seconds')
             job.save()
 
-logging.debug("# SchildCafe Barista finished at "+datetime.now().isoformat())
+logging.debug("# SchildCafe Barista finished at " + datetime.now().isoformat())
 
 sys.exit()
